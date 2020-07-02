e01a4d62fe075bafb528d093f9d21f2efeca9e28
Yessine KAMMOUN <67662149+codingamer@users.noreply.github.com>
2020-07-02 13:01:01 +0200

    Create TIM3_CAPTURE_MODE.h
    Declaring CAPTURE_MODE functions (piloted by the timer three)

---
+++ TIM3_CAPTURE_MODE/TIM3_CAPTURE_MODE.h
@@ -0,0 +1,9 @@
+#ifndef __TIM3_CAPTURE_MODE_H
+#define __TIM3_CAPTURE_MODE_H
+
+void Init_Capture_Mode(int);
+void Set_PB0(void);
+void Set_Capture_Mode_TIM3(int);
+int get_val(void);
+
+#endif /* __TIM3_CAPTURE_MODE_H */
